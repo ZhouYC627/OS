22d1246bc0d3b2f54dfde07c4998ae4d4cc26f12
Zhou Yichen <zhouycsf@gmail.com>
2016-04-03 17:39:12 +0800

    play

--- kernel/kernel/idt.c
+++ kernel/kernel/idt.c
@@ -34,6 +34,26 @@ set_trap(struct GateDescriptor *ptr, uint32_t selector, uint32_t offset, uint32_
 }
 
 /* 声明函数，这些函数在汇编代码里定义 */
+void irq0();
+void irq1();
+void irq14();
+void vec0();
+void vec1();
+void vec2();
+void vec3();
+void vec4();
+void vec5();
+void vec6();
+void vec7();
+void vec8();
+void vec9();
+void vec10();
+void vec11();
+void vec12();
+void vec13();
+void vec14();
+void vecsys();
+
 void test1();
 void test2();
 void irq_empty();
@@ -44,12 +64,35 @@ void init_idt() {
 	for (i = 0; i < NR_IRQ; i ++) {
 		set_trap(idt + i, SEG_KCODE, (uint32_t)irq_empty, DPL_KERN);
 	}
-    /*
-     * init your idt here
-     * 初始化 IDT 表, 为中断设置中断处理函数
-     */
-    set_intr(idt + 0x41, SEG_KCODE, (uint32_t)test1, DPL_KERN);
-    set_intr(idt + 0x42, SEG_KCODE, (uint32_t)test2, DPL_USER);
+	/*
+	 * init your idt here
+	 * 初始化 IDT 表, 为中断设置中断处理函数
+	 */
+	set_trap(idt + 0, SEG_KCODE, (uint32_t)vec0, DPL_KERN);
+	set_trap(idt + 1, SEG_KCODE, (uint32_t)vec1, DPL_KERN);
+	set_trap(idt + 2, SEG_KCODE, (uint32_t)vec2, DPL_KERN);
+	set_trap(idt + 3, SEG_KCODE, (uint32_t)vec3, DPL_KERN);
+	set_trap(idt + 4, SEG_KCODE, (uint32_t)vec4, DPL_KERN);
+	set_trap(idt + 5, SEG_KCODE, (uint32_t)vec5, DPL_KERN);
+	set_trap(idt + 6, SEG_KCODE, (uint32_t)vec6, DPL_KERN);
+	set_trap(idt + 7, SEG_KCODE, (uint32_t)vec7, DPL_KERN);
+	set_trap(idt + 8, SEG_KCODE, (uint32_t)vec8, DPL_KERN);
+	set_trap(idt + 9, SEG_KCODE, (uint32_t)vec9, DPL_KERN);
+	set_trap(idt + 10, SEG_KCODE, (uint32_t)vec10, DPL_KERN);
+	set_trap(idt + 11, SEG_KCODE, (uint32_t)vec11, DPL_KERN);
+	set_trap(idt + 12, SEG_KCODE, (uint32_t)vec12, DPL_KERN);
+	set_trap(idt + 13, SEG_KCODE, (uint32_t)vec13, DPL_KERN);
+	set_trap(idt + 14, SEG_KCODE, (uint32_t)vec14, DPL_KERN);
+
+	//system call int 0x80
+	set_trap(idt + 0x80, SEG_KCODE, (uint32_t)vecsys, DPL_USER);
+	
+	set_intr(idt + 0x0 , SEG_KCODE, (uint32_t)irq0, DPL_KERN);
+	set_intr(idt + 0x1 , SEG_KCODE, (uint32_t)irq1, DPL_KERN);
+	set_intr(idt + 14  , SEG_KCODE, (uint32_t)irq14, DPL_KERN);
+
+	set_intr(idt + 0x41, SEG_KCODE, (uint32_t)test1, DPL_KERN);
+	set_intr(idt + 0x42, SEG_KCODE, (uint32_t)test2, DPL_USER);
 
 	/* 写入IDT */
 	save_idt(idt, sizeof(idt));
