49974c525996535ccd4b368744387542ab6d77c1
Zhou Yichen <zhouycsf@gmail.com>
2016-04-04 18:32:13 +0800

    play

--- kernel/kernel/kvm.c
+++ kernel/kernel/kvm.c
@@ -42,7 +42,7 @@ enter_user_space(void) {
      * and use 'iret' to jump to ring3
      * 进入用户空间
      */
-     asm volatile("pushw %%ax" :: "a"(USEL(SEG_UCODE))); //ss
+     asm volatile("pushw %%ax" :: "a"(USEL(SEG_UDATA))); //ss
 	 asm volatile("pushl %%eax" :: "a"(0x200000));	//esp
 	 asm volatile("pushfl");	//eflags;
 	 asm volatile("pushw %%ax" :: "a" (USEL(SEG_UCODE))); //cs
