e500f606c2f7b49770abb888eac3f3a0624378a5
Zhou Yichen <zhouycsf@gmail.com>
2016-04-05 15:40:56 +0800

    play

--- lib/syscall.c
+++ lib/syscall.c
@@ -5,9 +5,9 @@
  * 库函数写在这
  */
 
-//static inline int32_t syscall(int num, int check, uint32_t a1,uint32_t a2)
+static inline int32_t syscall(int num, int check, uint32_t a1,uint32_t a2)
 //		uint32_t a3, uint32_t a4, uint32_t a5)
-static inline int32_t syscall(int num, ...)
+//static inline int32_t syscall(int num, ...)
 {
 	int32_t ret = 0;
 	//Generic system call: pass system call number in AX
@@ -24,8 +24,9 @@ static inline int32_t syscall(int num, ...)
 	/*Lab2 code here
 	  嵌入汇编代码，调用int $0x80
 	 */
-	int *args = &num;
-	asm volatile("int $0x80" : "=a"(ret) : "a"(args[0]), "b"(args[1]), "c"(args[2]), "d"(args[3]));
+	//int *args = &num;
+	//asm volatile("int $0x80" : "=a"(ret) : "a"(args[0]), "b"(args[1]), "c"(args[2]), "d"(args[3]));
+	asm volatile("int $0x80" : "=a"(ret) : "a"(num), "b"(check), "c"(a1), "d"(a2));
 	return ret;
 }
 /*
@@ -40,7 +41,7 @@ int printf(const char *format,...){
 	static char buf[256];
 	void *args = (void**)&format + 1;
 	int len = vsnprintf(buf, 256, format, args);
-	return syscall(4, 1, buf, len);
+	return syscall(4, 1, (uint32_t)buf, len);
 	
 } 
 
