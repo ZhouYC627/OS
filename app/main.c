ec19187c2d09747964c336b6be219b94990b07ae
Zhou Yichen <zhouycsf@gmail.com>
2016-04-08 21:09:47 +0800

    > play 141220162 Zhou YiChen Linux Ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:09:47 up  1:14,  3 users,  load average: 0.57, 0.66, 0.42

--- app/main.c
+++ app/main.c
@@ -16,7 +16,7 @@ uentry(void){
 	printf("%s %s%scome %co%s", "Hello,", "", "wel", 't', " ");
 	printf("%c%c%c%c%c! ", 'O', 'S', 'l', 'a', 'b');
 	printf("I'm the %s of %s. %s 0x%x, %s 0x%x. ", "body", "the game", "Bootblock loads me to the memory position of", 0x100000, "and Makefile also tells me that I'm at the location of", 0x100000);
-	printf("~!@#\()^&*()_+`1234567890-=...... ");
+	printf("~!@#\(^&*()_+`1234567890-=...... ");
 	printf("Now I will test your printf: ");
 	printf("%d + %d = %d, %d * %d = %d\n", 1, 1, 1 + 1, 123, 456, 123 * 456);
 	printf("%d, %d, %d, %d, %d, %d\n", 0, 0xffffffff, 0x80000000, 0xabcedf01, -32768, 102030);
