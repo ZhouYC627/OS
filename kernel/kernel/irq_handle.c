2b1385f5034e8684046600549783baf463b409ba
Zhou Yichen <zhouycsf@gmail.com>
2016-04-08 21:28:03 +0800

    > play 141220162 Zhou YiChen Linux Ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:28:03 up  1:32,  3 users,  load average: 0.95, 0.60, 0.51

--- kernel/kernel/irq_handle.c
+++ kernel/kernel/irq_handle.c
@@ -26,8 +26,8 @@ add_irq_handle(int irq, void (*func)(void) ){
     handles[irq] = ptr;
 }
 
-int row = 1;
-int column = 0;
+int row = 0;
+int column = 1;
 char ch;
 void scr_write(char c){
 	if (c == '\n' || column == 80){
